4372ce857118dfecdd6f28a581db06534ad018c0
natassia-nn <natassianguyen@gmail.com>
2024-09-25 09:48:00 -0700

    completed assignment (coins)

--- Coins.java
+++ Coins.java
@@ -2,9 +2,21 @@ public class Coins
 {
   public static void main(String[] args)
   {
-    // test code here
+    coinConverter(259);
   }
 
-  // write code for coinConverter
+  public static void coinConverter(int p)
+  {
+    int dollars = (p/100);
+    int quarters = (p%100/25);
+    int dimes = (p%100%25/10);
+    int nickels = (p%100%25%10/5);
+    int pennies = (p%100%25%10%5);
+    System.out.println("Dollar Bills: " + dollars);
+    System.out.println("Quarters: " + quarters);
+    System.out.println("Dimes: " + dimes);
+    System.out.println("Nickels: " + nickels);
+    System.out.println("Pennies: " + pennies);
+  }
 
 }
